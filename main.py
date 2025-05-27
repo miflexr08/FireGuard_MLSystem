ac7e35463c0f8b02f720cfe323506fab678edeb8
bitficker <shopper.tiago@gmail.com>
2025-05-27 20:46:10 -0300

    feat: implementação de ajuste de peso com derivativas

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # Implement weight correction for multiple weights
+# Create .gitignore
 # Implement alpha (observe behavior before implement it)
 # Create a domain class to encapsulate the Perceptron
 # Create Doc about criterias for fire propagation (more technical)
@@ -24,24 +25,51 @@ class Perceptron():
         self.weights = np.array(initial_weights)
         self.fire_propagation_chance = goal 
         self.error_limit = 0.02 # What is a good error limit?
+        self.alpha = 0.01
+
+        self.current_error = 0
+        self.bigger_error_times = 0
     
 
-    def calculate_error(self):
+    def make_prediction(self):
 
         try:
             prediction = np.dot(self.inputs, self.weights)
             delta_error = prediction - self.fire_propagation_chance
 
-            if delta_error < self.error_limit:
-                print("Function approximated")
-                print(f"Weight values: {self.weights}")
+            if delta_error <= self.error_limit:
+                return 1
 
-            error = (prediction - self.fire_propagation_chance) ** 2
-            print(f"Quadratic medium error: {error}")
-            # Explain 'Quadratic medium error' here:
+            delta_error = prediction - self.fire_propagation_chance
+            if delta_error > self.current_error:
+                self.bigger_error_times += 1
+                if self.bigger_error_times == 3:
+                    raise Exception("Error is growing...")
 
-        except:
-            print("Invalid input information")
+            mean_squared_error = delta_error ** 2
+            
+            print(f"Mean squared error: {mean_squared_error}")
+            # What is Mean squared error?
+            """
+            Mean squared error Explanation:
+            
+            """
+
+            # A new vector with the derivatives 
+            derivatives = np.multiply(self.inputs, delta_error)
+
+            print(f"derivatives: {derivatives}")
+
+            new_weights = self.weights - derivatives
+
+            print(f"new_weights: {new_weights}")
+
+            self.weights = new_weights
+
+            self.make_prediction()
+
+        except e:
+            print(f"Error: {e}")
 
 
         
