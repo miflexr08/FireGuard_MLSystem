07df5c8d0173b2bc1509f54d3b824cc5a2b4a8e7
Tiago Curi <tiago.curi@voxeldigital.com.br>
2025-06-24 14:07:22 -0300

    chore: improving concepts abstraction in code

--- MNIST_Training/main.py
+++ MNIST_Training/main.py
@@ -1,29 +1,41 @@
 from data_sets import *
 from neural_net import *
 
-# Test it in a function format (function aproximation concept)
+# Test it in a function format (function approximation concept)
 weights = [
-    #toes wlrec nfans
+    #toes #wlrec #nfans
     [0.1, 0.1, -0.3], #hurt
     [0.1, 0.2, 0.0], #win
     [0.0, 1.3, 0.1] #sad
 ]
-
 input_data = [toes[0], wlrec[0], nfans[0]]
-
 neural_net = NeuralNetwork(input_data, weights)
 
-# 1. Run prediction with current weights
-neural_net.run_prediction()
-print("current prediction: "+neural_net.current_prediction)
+# Notes:
+# 1. Move it inside the NeuralNetwork class
+# 2. Each prediction can have it's own LIMIAR_ERROR
+# 3. What about comparing the sum's of limiar errors
+# to the individual prediction errors (when data is tighly related)
+# Desta forma eu teria algo como erros individuais e um limiar de erro geral,
+# podendo calcular desvio médio padrão por feature (?)
+# Qual é o problema em comparar o limiar de erro com o erro quadrado médio?
+# Tomar nota das propriedades escondidas na multiplicação do input pelo erro
+while True:
 
-# 2. Calculate error and delta (gradient)
-neural_net.calculate_error(real_data=[hurt[0], win[0], sad[0]])
-print("current error: "+neural_net.current_error)
+    # 1. Run prediction with current weights
+    neural_net.run_prediction()
+    print("current prediction: "+neural_net.current_prediction)
 
-print("current weights: "+ neural_net.weights)
-# 3. Adjust the weights
-neural_net.gradient_descent()
+    if neural_net.converged():
+        break
+
+    # 2. Calculate error and delta (gradient)
+    delta_error = neural_net.calculate_error(real_data=[hurt[0], win[0], sad[0]])
+    print("current error: "+neural_net.current_error)
+
+    print("current weights: "+ neural_net.weights)
+    # 3. Adjust the weights
+    neural_net.gradient_descent(delta_error)
 
 
 
--- MNIST_Training/neural_net.py
+++ MNIST_Training/neural_net.py
@@ -8,51 +8,63 @@ class NeuralNetwork:
         self.current_prediction = [0, 0, 0]
         self.current_error = None
 
-        self.ALPHA = 0.01
+        self.ALPHA = 0.01 # use it
+        self.LIMIAR_ERROR = 0.002
 
     def run_prediction(self):
         for i in range(self.input):
-
             self.current_prediction[i] = self.weight_sum(self.weights[i])
-
-        print(f"prediction: {self.current_prediction}")
 
     # Each element in weights[i] corresponds to the connection strength between input neurons and the ith output neuron
     def weight_sum(self, weights, inputs=[]):
         return weights[0] * self.input[0] + weights[1] * self.input[1] + weights[2] * self.input[2]
 
     def calculate_error(self, real_data):
-        pass
+        error = [0, 0, 0]
+        for i in range(real_data):
+            self.current_error[i] = self.current_prediction[i] - real_data[i]
+            error[i] = self.current_error[i] ^ 2
 
-    def vect_mat_mul(self, vect, matrix):
-        assert(len(vect) == len(matrix))
-
-        for i in range(len(vect)):
-            output = [0, 0, 0]
-            output[i] = self.w_sum(vect, matrix[i])
-
-        return output
-
-    def zeros_matrix(self, size_a, size_b):
-        pass
-
-    def outer_prod(self, vec_a, vec_b):
-        out = self.zeros_matrix(len(vec_a), len(vec_b))
-        for i in range(len(vec_a)):
-            for j in range(len(vec_b)):
-                out[i][j] = vec_a[i] * vec_b[j]
-
-        return out
+        return error
 
     def gradient_descent(self):
-        pass
+        for i in range(input):
+            error = self.current_error[i]
+            gradients = self.input*error
+
+            self.weights[i] - gradients
+
+    def converged(self) -> bool:
+        if (self.current_error[0] <= self.LIMIAR_ERROR and
+                    self.current_error[1] <= self.LIMIAR_ERROR and
+                        self.current_error[2] <= self.LIMIAR_ERROR):
+            print("Neural net has converged")
+            return True
+
+        print("Not converged yet")
+        return False
 
 
-# error = [0, 0, 0]
-# delta = [0, 0, 0]
-#
-# for i in range(len(real_output)):
-#     error[i] = (pred[i] - real_output[i]) ** 2
-#     delta[i] = pred[i] - real_output[i]
-#
+    # def vect_mat_mul(self, vect, matrix):
+    #     assert(len(vect) == len(matrix))
+    #
+    #     for i in range(len(vect)):
+    #         output = [0, 0, 0]
+    #         output[i] = self.w_sum(vect, matrix[i])
+    #
+    #     return output
+
+    #def outer_prod(self, vec_a, vec_b):
+    #     out = self.zeros_matrix(len(vec_a), len(vec_b))
+    #     for i in range(len(vec_a)):
+    #         for j in range(len(vec_b)):
+    #             out[i][j] = vec_a[i] * vec_b[j]
+    #
+    #     return out
+
+    # def zeros_matrix(self, size_a, size_b):
+    #     pass
+
+
+# ANALYSE
 # weight_deltas = outer_prod(input_data, delta)
